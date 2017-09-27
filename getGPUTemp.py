e86a9a665d16e0b64ec626353484e551b8494c3b
Prathamesh Tajane <ptajane@gmail.com>
2017-09-27 14:02:58 -0400

    bugfix:remove split() on s2_out

--- getGPUTemp.py
+++ getGPUTemp.py
@@ -2,4 +2,5 @@ import sys
 import subprocess
 from subprocess import call
 s2_out=call(["vcgencmd","measure_temp"])
-print(s2_out.split("=")[1].split("'")[0])
+#print(s2_out.split("=")[1].split("'")[0])
+print(s2_out)
